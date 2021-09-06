fd4a12d94b87027aea63762b9bfcd77c1cbf661d
Tejas Jadhav <tjadhav95@gmail.com>
2021-09-07 01:46:47 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth"
 
 
 let config = {
-  apiKey: "AIzaSyBcCiSCJj2ZBsQz-jOSCaRlmK9iEJFDBXQ",
-  authDomain: "meditation-app-db.firebaseapp.com",
-  databaseURL: "https://meditation-app-db-default-rtdb.firebaseio.com/",
-  projectId: "meditation-app-db",
-  storageBucket: "meditation-app-db.appspot.com",
-  messagingSenderId: "650890334603",
-  appId: "1:650890334603:web:5fde5c580b3608c2239f8e"
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXX-app-db.firebaseapp.com",
+  databaseURL: "https://XXXXXXXXXXX-app-db-default-rtdb.firebaseio.com/",
+  projectId: "XXXXXXXXXXX-app-db",
+  storageBucket: "XXXXXXXXXXX-app-db.appspot.com",
+  messagingSenderId: "XXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXXXX"
 };
 
 const app = firebase.initializeApp(config)
